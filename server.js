a4a4db90e7fd274f64b3702072b227a29d6c3f95
pekny <91953677+PeknyBoss@users.noreply.github.com>
2025-02-17 14:16:40 +0100

    Szerver beallitasa
    mysql szever kapcsolatanak létrehozása. get nmegírása

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
-const bodyparser= require("body-parser");
+const bodyparser = require("body-parser");
  
 app.use(cors());
 app.use(bodyparser.json());
  
+const db = mysql.createConnection(
+{
+ user: "root",
+ host: "localhost",
+ port: "3307",
+ password: "",
+ database: "teliolimpia"
  
-app.listen(3000, ()=>{
-    console.log('A szerver a 3000 porton fut!')
+})
+//A szerver futásásak ellenőrzése
+ 
+app.get("/",(req,res) => {
+    res.send("A szerver működik!")
+})
+ 
+app.listen(3000, () => {
+    console.log('A szerver a 3000-es porton fut!')
 })
