d8cc69a4aab4f649c491b1bec4fefc1176a90b80
pekny <91953677+PeknyBoss@users.noreply.github.com>
2025-02-17 14:32:27 +0100

    select használat

--- server.js
+++ server.js
@@ -3,25 +3,44 @@ const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const bodyparser = require("body-parser");
- 
+
 app.use(cors());
 app.use(bodyparser.json());
- 
+
 const db = mysql.createConnection(
-{
- user: "root",
- host: "localhost",
- port: "3307",
- password: "",
- database: "teliolimpia"
- 
-})
+    {
+        user: "root",
+        host: "localhost",
+        port: "3307",
+        password: "",
+        database: "teliolimpia"
+
+    })
 //A szerver futásásak ellenőrzése
- 
-app.get("/",(req,res) => {
+
+app.get("/", (req, res) => {
     res.send("A szerver működik!")
 })
- 
+app.get("/v", (req, res) => {
+    const sql = SELECT * FROM`versenyzok`;
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+}
+)
+app.get("/v6/:id", (req, res) => {
+    const sql = "SELECT * FROM `versenyzok` WHERE ID = 6";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+)
+
+}
+)
+
 app.listen(3000, () => {
     console.log('A szerver a 3000-es porton fut!')
 })
